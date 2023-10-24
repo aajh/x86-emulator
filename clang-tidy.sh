a474c625e802b16b2d1de283fb013213f4bd8865
Aapo Hanski <aapo.hanski@gmail.com>
2023-10-24 15:38:59 +0300

    Script for running clang-tidy

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+echoerr() { printf "%s\n" "$*" >&2; }
+
 if hash clang-tidy 2> /dev/null; then
-    echo "Has clang-tidy"
+    clang_tidy_cmd=clang-tidy
 elif /usr/local/opt/llvm/bin/clang-tidy --version > /dev/null 2>&1; then
-    echo "Has /usr/local/opt/llvm/bin/clang-tidy"
+    clang_tidy_cmd=/usr/local/opt/llvm/bin/clang-tidy
 else
-    echo "Has neither"
-    exit
+    echoerr "clang-tidy not found from PATH or /usr/local/opt/llvm/bin/"
+    exit 1
 fi
 
-echo "ASD"
+if [ ! -f build/compile_commands.json ]; then
+    echo "build/compile_commands.json not found, creating build..."
+    mkdir build && cd $_
+    cmake ..
+    cd ..
+    if [ ! -f build/compile_commands.json ]; then
+        echoerr "couldn't create build/compile_commands.json"
+        exit 1
+    fi
+fi
+
+arguments="$@"
+if [ -z "$1" ]; then
+    arguments="src/*.cpp"
+fi
+
+$clang_tidy_cmd -checks="-*,bugprone-*,cppcoreguidelines-*,clang-analyzer-*" -p build $arguments
